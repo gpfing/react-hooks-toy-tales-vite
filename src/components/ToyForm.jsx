47607647beb9e5c33d10006c23509491f3f9e838
Garrett Pfingston <gpfingston@gmail.com>
2025-11-11 15:43:00 -0600

    added CRUD to toy tales

--- src/components/ToyForm.jsx
+++ src/components/ToyForm.jsx
@@ -1,13 +1,52 @@
-import React from "react";
+import React, {useState} from "react";
 
-function ToyForm() {
+function ToyForm({addToy}) {
+  const [formData, setFormData] = useState({
+    name: "",
+    image: ""
+  })
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    const newToy = {
+      ...formData,
+      likes: 0,
+    }
+
+    fetch("http://localhost:3001/toys", {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(newToy)
+    })
+      .then(r => {
+        if (!r.ok) {throw new Error("failed to create new toy")}
+        return r.json()
+      })
+      .then(newToy => {
+        addToy(newToy)
+        setFormData({
+          name: "",
+          image: "",
+        })
+      })
+      .catch(error => console.log(error.message))
+  }
+
+  const handleChange = e => {
+    setFormData(previousData => ({
+      ...previousData, 
+      [e.target.name]: e.target.value
+    }))
+  }
   return (
     <div className="container">
-      <form className="add-toy-form">
+      <form className="add-toy-form" onSubmit={handleSubmit}>
         <h3>Create a toy!</h3>
         <input
           type="text"
           name="name"
+          value={formData.name}
+          onChange={handleChange}
           placeholder="Enter a toy's name..."
           className="input-text"
         />
@@ -15,6 +54,8 @@ function ToyForm() {
         <input
           type="text"
           name="image"
+          value={formData.image}
+          onChange={handleChange}
           placeholder="Enter a toy's image URL..."
           className="input-text"
         />
